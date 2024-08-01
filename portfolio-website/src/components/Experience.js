92086239b471ddeb690ae6f41bc4ee21fc9481d8
Vaibhav Monpara <vaibhav98patel@gmail.com>
2024-07-31 21:52:31 -0700

    changes

--- portfolio-website/src/components/Experience.js
+++ portfolio-website/src/components/Experience.js
@@ -8,17 +8,29 @@ const Experience = () => {
         <h2>Experience</h2>
         <div className="experience-grid">
           <div className="card">
-            <h3>Midocean Technologies India</h3>
-            <h4>Sr. Software Engineer (Jan 2022 – May 2022)</h4>
+          <h3>
+                <a href="https://www.midocean.tech" target="_blank" rel="noopener noreferrer">
+                  Midocean Technologies
+                </a>
+              </h3>
+            <h4>Software Engineer (Jan 2022 – May 2022)</h4>
             <p>Led a project from initiation to completion post-client pitch, achieving timely delivery and a 15% boost in client satisfaction...</p>
           </div>
           <div className="card">
-            <h3>Midocean Technologies India</h3>
+          <h3>
+                <a href="https://www.midoceantecn.tech" target="_blank" rel="noopener noreferrer">
+                  Midocean Technologies
+                </a>
+              </h3>
             <h4>Jr. Software Engineer (Jan 2021 – Dec 2021)</h4>
             <p>Deployed a comprehensive Brass Manufacturing Module using Python, JavaScript, HTML, CSS, jQuery, XML, and integrated it with a Flutter mobile app...</p>
           </div>
           <div className="card">
-            <h3>Midocean Technologies India</h3>
+          <h3>
+                <a href="https://www.midocean.tech" target="_blank" rel="noopener noreferrer">
+                  Midocean Technologies
+                </a>
+              </h3>
             <h4>Internship Trainee (Oct 2020 - Dec 2020)</h4>
             <p>Acquired practical Python proficiency, enabling creation of efficient and streamlined code for modules...</p>
           </div>
--- portfolio-website/src/components/Header.js
+++ portfolio-website/src/components/Header.js
@@ -11,12 +11,12 @@ const Header = () => {
         </div>
         <nav>
           <ul>
-            <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-            <li><Link to="experience" smooth={true} duration={500}>Experience</Link></li>
-            <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
-            <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
-            <li><Link to="education" smooth={true} duration={500}>Education</Link></li>
-            <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+            <li><Link to="home" smooth={true} duration={650}>Home</Link></li>
+            <li><Link to="experience" smooth={true} duration={650}>Experience</Link></li>
+            <li><Link to="projects" smooth={true} duration={650}>Projects</Link></li>
+            <li><Link to="skills" smooth={true} duration={650}>Skills</Link></li>
+            <li><Link to="education" smooth={true} duration={650}>Education</Link></li>
+            <li><Link to="contact" smooth={true} duration={650}>Contact</Link></li>
           </ul>
         </nav>
       </div>
