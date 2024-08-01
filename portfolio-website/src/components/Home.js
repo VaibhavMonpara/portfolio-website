c92c180c169de90d6e302aa1cbbd9f255c509d75
Vaibhav Monpara <vaibhav98patel@gmail.com>
2024-07-31 21:04:21 -0700

    Changes in home page

--- portfolio-website/src/components/Home.js
+++ portfolio-website/src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../styles/Home.css';
-import profileImage from '../assets/profile-image.jpg'; // Ensure this path is correct
+import profileImage from '../assets/profile-image.jpg';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { SiX } from 'react-icons/si';
 
 const Home = () => {
   return (
@@ -8,8 +10,18 @@ const Home = () => {
       <div className="home-content">
         <img src={profileImage} alt="Profile" className="profile-image" />
         <h1>Hi, I'm Vaibhav Monpara</h1>
-        <p>Software engineer by day, bug hunter by night. Fueled by chai, code, and an uncanny ability to turn caffeine into features. Ready to debug the universe!</p>
-        <a href="https://www.linkedin.com/in/vaibhav-monpara" target="_blank" rel="noopener noreferrer" className="btn">Connect on LinkedIn</a>
+        <p className='home-data'>Software engineer by day, bug hunter by night. Fueled by chai, code, and an uncanny ability to turn caffeine into features. Ready to debug the universe!</p>
+        <div className="social-links">
+          <a href="https://www.linkedin.com/in/vaibhav-monpara" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/VaibhavMonpara" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <FaGithub />
+          </a>
+          <a href="https://x.com/VaibhavMonpara" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <SiX />
+          </a>
+        </div>
       </div>
     </section>
   );
